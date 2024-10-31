57a95c16d13fca91033d12471c3d4f9ee6571727
K-Yevhenii <ya.korniakov@gmail.com>
2024-10-31 13:49:45 +0200

    Rewrite styles to flexbox (#1)
    * fix header
    
    * rewrite-styles-to-flexbox
    
    * rewrite-styles-to-flexbox
    
    * rewrite-styles-to-flexbox
    
    * finish flex-box

--- js/countdown.js
+++ js/countdown.js
@@ -1,10 +1,10 @@
-window.onload = function() {   
+window.onload = function () {
     function Clock(deadline) {
-        this.getTotalTime = function() {
+        this.getTotalTime = function () {
             return Date.parse(deadline) - Date.parse(new Date());
         }
 
-        this.updateTime = function() {
+        this.updateTime = function () {
             return {
                 sec: Math.floor((this.getTotalTime() / 1000) % 60),
                 min: Math.floor((this.getTotalTime() / 1000 / 60) % 60),
@@ -13,30 +13,30 @@ window.onload = function() {
             }
         }
 
-        this.addZero = function(digit) {
+        this.addZero = function (digit) {
             return digit < 10 ? '0' + digit : digit;
-        } 
+        }
 
-        this.printDigits = function(where) {
+        this.printDigits = function (where) {
             document.getElementById(where[0]).innerHTML = this.addZero(this.updateTime().sec);
             document.getElementById(where[1]).innerHTML = this.addZero(this.updateTime().min);
-        	document.getElementById(where[2]).innerHTML = this.addZero(this.updateTime().hours);
+            document.getElementById(where[2]).innerHTML = this.addZero(this.updateTime().hours);
             document.getElementById(where[3]).innerHTML = this.addZero(this.updateTime().days);
         }
 
-        this.elapsed = function(where, message) {
-        	document.getElementById(where).innerHTML = '<p class= elapsed>'+ message + '</p>'
+        this.elapsed = function (where, message) {
+            document.getElementById(where).innerHTML = '<p class= elapsed>' + message + '</p>'
             console.log(message);
         }
     };
 
-    var timer = new Clock(new Date(2017, 9, 21, 9, 47));    
-    var timerStart = setInterval(function() {
-		if(timer.getTotalTime() > 0) {
-			timer.printDigits(['sec', 'min', 'hours', 'days']);
-		} else {
-			clearInterval(timerStart);
-			timer.elapsed('countdown', 'Join us at next meetup');
-		}
+    var timer = new Clock(new Date(2024, 10, 1, 9, 47));
+    var timerStart = setInterval(function () {
+        if (timer.getTotalTime() > 0) {
+            timer.printDigits(['sec', 'min', 'hours', 'days']);
+        } else {
+            clearInterval(timerStart);
+            timer.elapsed('countdown', 'Join us at next meetup');
+        }
     }, 1000);
 }
