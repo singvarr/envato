6ab771a6e723725340bfa28e88bc4577aeb3755f
K-Yevhenii <ya.korniakov@gmail.com>
2025-03-04 12:53:49 +0200

    improve clock (#12)

--- js/countdown.js
+++ js/countdown.js
@@ -1,42 +1,52 @@
 window.onload = function () {
-    function Clock(deadline) {
-        this.getTotalTime = function () {
-            return Date.parse(deadline) - Date.parse(new Date());
-        }
+    class Clock {
+        #deadline;
 
-        this.updateTime = function () {
-            return {
-                sec: Math.floor((this.getTotalTime() / 1000) % 60),
-                min: Math.floor((this.getTotalTime() / 1000 / 60) % 60),
-                hours: Math.floor((this.getTotalTime() / 1000 / 3600) % 24),
-                days: Math.floor((this.getTotalTime() / 1000 / 86400))
-            }
-        }
-
-        this.addZero = function (digit) {
+        static #addZero(digit) {
             return digit < 10 ? '0' + digit : digit;
         }
 
-        this.printDigits = function (where) {
-            document.getElementById(where[0]).innerHTML = this.addZero(this.updateTime().sec);
-            document.getElementById(where[1]).innerHTML = this.addZero(this.updateTime().min);
-            document.getElementById(where[2]).innerHTML = this.addZero(this.updateTime().hours);
-            document.getElementById(where[3]).innerHTML = this.addZero(this.updateTime().days);
+        constructor(deadline) {
+            this.#deadline = deadline;
         }
 
-        this.elapsed = function (where, message) {
+        #getTotalTime() {
+            return Date.parse(this.#deadline) - Date.parse(new Date());
+        }
+
+        #updateTime() {
+            return {
+                sec: Math.floor((this.#getTotalTime() / 1000) % 60),
+                min: Math.floor((this.#getTotalTime() / 1000 / 60) % 60),
+                hours: Math.floor((this.#getTotalTime() / 1000 / 3600) % 24),
+                days: Math.floor((this.#getTotalTime() / 1000 / 86400))
+            }
+        }
+
+        #printDigits(where) {
+            document.getElementById(where[0]).innerHTML = Clock.#addZero(this.#updateTime().sec);
+            document.getElementById(where[1]).innerHTML = Clock.#addZero(this.#updateTime().min);
+            document.getElementById(where[2]).innerHTML = Clock.#addZero(this.#updateTime().hours);
+            document.getElementById(where[3]).innerHTML = Clock.#addZero(this.#updateTime().days);
+        }
+
+        #elapsed(where, message) {
             document.getElementById(where).innerHTML = '<p class=\'countdown__elapsed\'>' + message + '</p>'
             console.log(message);
         }
+
+        run() {
+            const timerStart = setInterval(() => {
+                if (this.#getTotalTime() > 0) {
+                    this.#printDigits(['sec', 'min', 'hours', 'days']);
+                } else {
+                    clearInterval(timerStart);
+                    this.#elapsed('countdown', 'Join us at next meetup');
+                }
+            }, 1000);
+        }
     };
 
-    var timer = new Clock(new Date(2024, 11, 1, 9, 47));
-    var timerStart = setInterval(function () {
-        if (timer.getTotalTime() > 0) {
-            timer.printDigits(['sec', 'min', 'hours', 'days']);
-        } else {
-            clearInterval(timerStart);
-            timer.elapsed('countdown', 'Join us at next meetup');
-        }
-    }, 1000);
+    const timer = new Clock(new Date(2025, 11, 1, 9, 47));
+    timer.run()
 }
